7c5b90398b5d2f04d733492128d3b0db6187f021
DRXLAR <sycarion.x@gmail.com>
2024-06-28 16:10:44 +0200

    Update Starter.py

--- src/Starter.py
+++ src/Starter.py
@@ -4,7 +4,7 @@ from rich.console import Console
 console = Console()
 tasks = [f"Successfully launched! " for n in range(1, 2)]
 
-with console.status("[bold green]Launch DDos please wait a few seconds ...[/bold green]") as status:
+with console.status("[bold green]Inciando A-Dos Protocol loading ...[/bold green]") as status:
     while tasks:
         task = tasks.pop(0)
         sleep(5)
